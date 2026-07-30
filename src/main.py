7ff1b6db8e4bfeb2a681595c6fbe77c4c33cb9d8
jangsoopark-codyssey <jangsoopark.codyssey@gmail.com>
2026-07-30 23:01:54 +0900

    argument parser

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from common import definitions
 from common import dotenv
 
+import argparse
 import sys
 import os
 
@@ -8,10 +9,31 @@ import os
 dotenv.load_dotenv(path=os.path.join(definitions.project_root, 'data', ".env"), override=True)
 
 
+def parse_args():
+    
+    parser = argparse.ArgumentParser(description="Process some integers.")
+    parser.add_argument('--data-file-path', type=str, help='Path to the data file')
+    parser.add_argument('--data-file-name', type=str, help='Name of the data file')
+
+    args = parser.parse_args()
+
+    # Set environment variables based on command-line arguments
+    if args.data_file_path:
+        os.environ['DATA_FILE_PATH'] = args.data_file_path
+    if args.data_file_name:
+        os.environ['DATA_FILE_NAME'] = args.data_file_name
+
+    return args
+
+
 def main():
+    args = parse_args()
+
     print(definitions.project_root)
     print(os.getenv('DATA_FILE_PATH'))
     print(os.getenv('DATA_FILE_NAME'))
+
+    return 0
 
 
 if __name__ == "__main__":
